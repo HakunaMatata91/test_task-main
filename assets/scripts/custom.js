318fa8f39a1e17410d5d1c1fcc385ad56657d42c
HakunaMatata <kononchuk.elena91@gmail.com>
2023-10-09 18:56:12 +0300

    finish test task for HolyAff

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -31,7 +31,7 @@ class Comment {
       const commentStatusDiv = document.createElement("div");
       commentStatusDiv.className = "comment-status";
       commentStatusDiv.innerHTML = `
-         <span>Curte·comente <img src="../images/like.png" width="15px" height="15px" alt="Like Image"> ${reactionCount} · </span>
+         <span>Curte·comente <img src="./assets/images/like.png" width="15px" height="15px" alt="Like Image"> ${reactionCount} · </span>
          <small><u>${timeAgo}</u></small>
       `;
       return commentStatusDiv;
@@ -61,9 +61,9 @@ const commentsData = [
    // Array of comment objects with user information
    {
       id: "comment0",
-      avatar: "../images/1.gif",
-      avatarWebP: "../images/1.webm",
-      avatarMp4: "../images/1.mp4",
+      avatar: "./assets/images/1.gif",
+      avatarWebP: "./assets/images/1.webm",
+      avatarMp4: "./assets/images/1.mp4",
       name: "Inês",
       commentText: "Eu ganhei nada",
       reactionCount: 29,
@@ -71,8 +71,8 @@ const commentsData = [
    },
    {
       id: "comment1",
-      avatar: "../images/4.jpg",
-      avatarWebP: "../images/4.webp",
+      avatar: "./assets/images/4.jpg",
+      avatarWebP: "./assets/images/4.webp",
       name: "FernandoPessoa",
       commentText: "Eu gostei essas promoções",
       reactionCount: 9,
@@ -80,8 +80,8 @@ const commentsData = [
    },
    {
       id: "comment2",
-      avatar: "/assets/images/5.jpg",
-      avatarWebP: "/assets/images/5.webp",
+      avatar: "./assets/images/5.jpg",
+      avatarWebP: "./assets/images/5.webp",
       name: "Maria",
       commentText: "Já recebi minha iPhone 11 Pro. Agradeço muito!",
       reactionCount: 22,
@@ -89,8 +89,8 @@ const commentsData = [
    },
    {
       id: "comment3",
-      avatar: "/assets/images/6.jpg",
-      avatarWebP: "/assets/images/6.webp",
+      avatar: "./assets/images/6.jpg",
+      avatarWebP: "./assets/images/6.webp",
       name: "Lucas",
       commentText: "Eu achava que era uma brincadeira, mas meu iPhone 11 Pro do Apple chegou hoje de manhã",
       reactionCount: 36,
